2b96c14efa90d8a7d5feb5d63342b6f1400399d1
Elijah Lucian <elijahlucian@gmail.com>
2021-06-17 05:03:33 -0600

    🌌🧠

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+const buttonEl = document.querySelector('.trigger') as HTMLButtonElement
 console.log('solar synth!')
+console.log('button', buttonEl)
 
 type Series = {
   name: string
@@ -25,6 +27,9 @@ const main = async () => {
   // create audio context
   const dataEl = document.querySelector('.data')
   let n = 0
+  // get min-max
+  // split time into days
+
   for (const [ts, val] of points) {
     const rowEl = document.createElement('div')
     rowEl.className = 'row'
@@ -33,7 +38,7 @@ const main = async () => {
     const tsEl = document.createElement('div')
     const valEl = document.createElement('div')
     numEl.innerText = n.toString().padStart(4, '0')
-    tsEl.innerText = ts + ''
+    tsEl.innerText = Math.floor(ts / 100000000000) - 16200000 + ''
     valEl.innerText = val + ''
     rowEl.appendChild(numEl)
     rowEl.appendChild(tsEl)
@@ -42,6 +47,50 @@ const main = async () => {
     dataEl.appendChild(rowEl)
     n++
   }
+
+  buttonEl.addEventListener('mousedown', createSynth)
+}
+
+const createCtx = (): AudioContext | null => {
+  try {
+    return new AudioContext()
+  } catch (e) {
+    alert('Web Audio API is not supported in this browser')
+    return null
+  }
+}
+
+const createSynth = async () => {
+  console.log('playing')
+  const context = createCtx()
+  if (!context) return
+
+  //create nodes
+  const osc = context.createOscillator() //create in event listener so we can press the button more than once
+  osc.frequency.value = 220
+  const masterGain = context.createGain()
+  const analyser = context.createAnalyser()
+
+  //routing
+  masterGain.connect(analyser)
+  analyser.connect(context.destination)
+
+  const real = new Float32Array([
+    0, 0.4, 0.4, 1, 1, 1, 0.3, 0.7, 0.6, 0.5, 0.9, 0.8,
+  ])
+  // const real = new Float32Array([0, 0, 1, 0, 1, 1, 0.5, 0.2, 1])
+  const imag = new Float32Array(real.length)
+  // const wavetable = context.create
+  const customWave = context.createPeriodicWave(real, imag) // cos,sine
+
+  osc.setPeriodicWave(customWave)
+
+  osc.connect(masterGain)
+  osc.start()
+
+  buttonEl.addEventListener('mouseup', () => {
+    osc.stop()
+  })
 }
 
 main()
