f46f5e7a797c0697bb1c34f8867020175f28cd09
Elijah Lucian <elijahlucian@gmail.com>
2021-06-19 11:07:15 -0600

    getting there...

--- audio-crush/src/lib.rs
+++ audio-crush/src/lib.rs
@@ -11,17 +11,21 @@ pub fn greet(name: &str) {
     alert(&format!("Hello, {}", name))
 }
 
-pub fn genSlopes(arr: Vec<f32>) -> Vec<f32> {
-    let ln = arr.len();
+// private
+fn genSlopes(arr: Vec<f32>) -> Vec<f32> {
+    let _ln = arr.len();
     let mut slopes = vec![];
     slopes.push(1.2);
     return slopes;
 }
 
+pub fn resample(frame: i32, arr: Vec<f32>) {}
+
 #[wasm_bindgen]
 pub fn remap(arr: Vec<f32>, ln: i32) -> Vec<f32> {
     lawg(ln);
-    return arr;
+    let slopes = genSlopes(arr);
+    return slopes;
 }
 
 // Crush down sample into vector map
